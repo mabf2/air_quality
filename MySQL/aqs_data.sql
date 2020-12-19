96376ae35789ab5e65ed1a0e15eec71eb5372811
Marcos Ferreira <maferreira.f2@gmail.com>
2020-12-19 00:13:49 -0300

    Add files via upload

--- MySQL/aqs_data.sql
+++ MySQL/aqs_data.sql
@@ -1,25 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5
--- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 18, 2020 at 10:58 PM
--- Server version: 10.3.25-MariaDB-0+deb10u1
--- PHP Version: 7.3.19-1~deb10u1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `esp_data`
+-- Database: `aqs_data`
 --
 
 -- --------------------------------------------------------
@@ -28,40 +8,36 @@ SET time_zone = "+00:00";
 -- Table structure for table `LogData`
 --
 
-CREATE TABLE `LogData` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `LogData` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `log` varchar(255) NOT NULL,
   `reading_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `SensorData`
 --
 
-CREATE TABLE `SensorData` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `SensorData` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `macaddress` varchar(12) NOT NULL,
   `sensor` varchar(10) NOT NULL,
   `feature` char(1) NOT NULL,
   `value` varchar(10) NOT NULL,
   `reading_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `SensorMAC`
 --
 
-CREATE TABLE `SensorMAC` (
+CREATE TABLE IF NOT EXISTS `SensorMAC` (
   `macaddress` varchar(12) NOT NULL,
   `active_since` date NOT NULL,
   `active_until` date NOT NULL,
   `location` varchar(30) NOT NULL,
   `checked` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for table `LogData`
@@ -84,19 +60,4 @@ ALTER TABLE `SensorMAC`
   ADD PRIMARY KEY (`macaddress`,`active_since`),
   ADD KEY `active_since` (`active_since`,`active_until`);
 
---
--- AUTO_INCREMENT for table `LogData`
---
-ALTER TABLE `LogData`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `SensorData`
---
-ALTER TABLE `SensorData`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=184428;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
